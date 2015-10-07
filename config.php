e22b66f5a71fb73f078353d03d3082d7616e6a6d
Ernesto Tarrillo <etarrillo.pe@gmail.com>
2015-10-07 18:25:31 -0500

    agregando archivos moodle al repositorio

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'pv_db';
+$CFG->dbuser    = 'pv_user';
+$CFG->dbpass    = '123';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://localhost/pv';
+$CFG->dataroot  = '/Users/etarrillo/dev/moodledatas/moodledatapv';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
